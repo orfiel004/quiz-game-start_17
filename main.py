91ab2dad33cfd0ea5a5b1d121fb1b04155157b16
Orfiel <pawel.sleczek@gmail.com>
2023-07-18 23:40:35 +0200

    assking the question

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from data import question_data
 question_bank = []
 for question in question_data:
     question_text = question["text"]
+    # from dictionary with key "text" assign value in to question_text
     question_answer = question["answer"]
     newQuestion = Question(question_text, question_answer)
+    # create new object Question witch attribute from question_text variable
     question_bank.append(newQuestion)
 
 # print(question_bank[3].text)
